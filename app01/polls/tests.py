b2e85217a3ddd24351a004c2f583ac1eb8f91f8d
Ryosuke Sakai <pussade0818@gmail.com>
2022-09-26 22:32:37 +0900

    fail test

--- app01/polls/tests.py
+++ app01/polls/tests.py
@@ -19,7 +19,7 @@ class QuestionModelTests(TestCase):
         """
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
+        self.assertIs(future_question.was_published_recently(), True)
 
     def test_was_published_recently_with_old_question(self):
         """
